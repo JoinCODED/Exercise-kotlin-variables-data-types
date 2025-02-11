27d1ce914314ed48f315b8bab36a6b4f7102016e
Abdulwahab-Alawadhi <abdulwahab.alawadhi56@gmail.com>
2025-02-11 18:43:01 +0300

    5th task done

---
+++ data_types.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    var temp: Double = 36.5
+    var raining: Boolean = false
+    var letter: Char = 'A'
+
+    println(
+        "The temperature is $temp degrees.\n" +
+                "Is it raining? $raining\n" +
+                "The first letter of my name is $letter.\n"
+    )
+
+}
