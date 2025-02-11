440994dafe87e32ca1a8cc2399d4e04480191344
Zainab 27 <zainabalsaffar03@gmail.com>
2025-02-11 18:48:59 +0300

    Variables file added

---
+++ Variables/src/Main.kt
@@ -0,0 +1,7 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var name:String = "Zainab AlSaffar"
+    var age:Int = 21
+    println("My name is $name and I am $age years old")
+}
