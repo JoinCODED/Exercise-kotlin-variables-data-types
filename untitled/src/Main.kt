865843e84ad92edd730c50087371647115d43b3e
deemaalbahri <deemamalbahri@gmail.com>
2025-02-11 18:58:59 +0300

    task done

---
+++ untitled/src/Main.kt
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val name: String = "Deema"
+    val age: Int = 24
+
+    println("My name is $name and I am $age years old.")
+
+}
