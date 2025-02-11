6218cc57ffc5c6fcca978a1edc42142dd4a867c1
Sarah <sarahalenezi96@outlook.com>
2025-02-11 19:32:24 +0300

    Added Kotlin data types exercise

---
+++ task/src/data_types.kt
@@ -0,0 +1,8 @@
+fun main(){
+    var double = 37.4
+    var boolean = "true"
+    var char = 'S'
+    println ("The temperature is $double degrees\n Is it raining? $boolean \n The first letter of my name is $char ")
+
+
+}
