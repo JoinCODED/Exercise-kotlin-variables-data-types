fa97f774bcc5f9752a94b0ef4b8c644535354219
Njood <nejood03@gmail.com>
2025-02-11 19:41:05 +0300

    Added type inference exercise

---
+++ Variable/src/type_inference.kt
@@ -0,0 +1,9 @@
+fun main() {
+
+    val city = "New York"
+
+    val temperature = 22.5
+
+    println("City: $city, Type: ${city.javaClass}")
+    println("Temperature: $temperature, Type: ${temperature.javaClass}")
+}
