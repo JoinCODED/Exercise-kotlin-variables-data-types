979fdb6a200f1f4b81c85bf01742821f1012a31f
Bashayer770 <Beshow95@gmail.com>
2025-02-11 18:44:02 +0300

    city

---
+++ type_inference.kt
@@ -0,0 +1,9 @@
+fun main(){
+    var city = "New York"
+    var tempreture = 22.5
+
+
+    println("The type of $city is class" + city.javaClass.kotlin +
+            "The type of $tempreture is class" + tempreture.javaClass.kotlin)
+
+}
