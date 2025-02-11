e30b8f86f7db4ef8b661e5b5809b0297644d5f51
dsh7x <dshx7x@gmail.com>
2025-02-11 18:52:56 +0300

    Added type inference exercise

---
+++ Dalal Al Shatti/type_inference.kt
@@ -0,0 +1,6 @@
+fun main () {
+    val city = "New York"
+    val temperature = 22.5
+    println("The type of city is ${city.javaClass.kotlin}")
+    println("The type of temperature is ${temperature.javaClass.kotlin}")
+}
