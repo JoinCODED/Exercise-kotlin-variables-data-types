46bb7b4dfd838ecc4bb6bfbc9e308a50bcf0f8c1
Dalal Bakhsh <DalalBakhsh98@gmail.com>
2025-02-11 19:26:38 +0300

    Added Kotlin data types exercise

---
+++ variabless/src/data_types.kt
@@ -0,0 +1,10 @@
+fun main()
+{
+    var temp: Double = 36.5
+    var bool: Boolean = false
+    var letter: Char  = 'A'
+
+    println("The temperture is $temp degrees.")
+    println("Is is raining? $bool")
+    println("The first letter of my name is $letter")
+}
