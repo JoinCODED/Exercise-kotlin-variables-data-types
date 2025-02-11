f17e0c61d837f60eb50a9aa414b2d5831237deba
ahjadi <alihussainaljadi@gmail.com>
2025-02-11 18:20:02 +0300

    Added Kotlin data types exercise

---
+++ data_types.kt
@@ -0,0 +1,9 @@
+fun main(){
+    val  temp: Double = 36.5
+    val isRaining: Boolean = false
+    val myFirstLetter: Char = 'A'
+
+    println("The temperature is $temp degrees.\n" +
+            "Is it raining? $isRaining\n" +
+            "The first letter of my name is $myFirstLetter.")
+}
