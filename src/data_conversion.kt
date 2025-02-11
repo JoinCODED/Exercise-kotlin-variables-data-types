18898aa0a8007c5b35389d7f87287d1664cf62ee
BarrakBM <barrak_mandani@live.com>
2025-02-11 18:03:00 +0300

    Added variable declaration exercise

---
+++ src/data_conversion.kt
@@ -0,0 +1,10 @@
+fun main(){
+
+    var numstring: String = "100"
+    var numInt = numstring.toInt()
+    var numString2: Int = 50
+    var numInt2 = numString2.toString()
+    println("numInt value is: $numInt")
+    println("numInt2 value is: $numInt2")
+
+}
