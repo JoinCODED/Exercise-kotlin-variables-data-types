7d126ca33f08d2a66484bd0bbc1e7c856ea6ae4c
fatemahAlMomen <fatemah.alm@hotmail.com>
2025-02-11 19:03:42 +0300

    Completed Kotlin Variables and Data Types exercises

--- val_vs_var.kt
+++ val_vs_var.kt
@@ -3,7 +3,7 @@ fun main() {
 
     var score = 10
     score = 20
-
+//val greeting = "hello kotlin"
     val greeting: String = "Hello, Kotlin!"
 //     greeting = "Chetory, kotlin" does not work
 }
