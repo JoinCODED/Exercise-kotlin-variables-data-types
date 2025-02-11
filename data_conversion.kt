30e0ff69f1401fd1d730dfd5396670a817d95d27
alrweashd <trkyabler@gmail.com>
2025-02-11 18:44:30 +0300

    Fourth task

---
+++ data_conversion.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+
+
+    var numString:String = "100"
+    var numInt = numString.toInt();
+
+    var numInt2 =50;
+    var numString2 = numInt2.toString();
+    // .javaClass.kotlin
+
+    println("numInt is: ${numInt}");
+    println("numString2 is:  ${numString2}");
+
+
+}
--- type_inference.kt
+++ type_inference.kt
@@ -2,11 +2,11 @@ fun main(args: Array<String>) {
 
     var city = "New York";
     var temperature = 22.5;
-    val greeting:String = "Hello, Kotlin!"
-    //greeting="ljsfil"
+
     // .javaClass.kotlin
 
     println("The type of city is ${city.javaClass.kotlin}.");
+    println("The type of temperature is ${temperature.javaClass.kotlin}.");
 
 
 }
