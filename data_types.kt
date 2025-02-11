554b4d46c6c9386b276b1d7a37cf728d9af22c08
Bashayer770 <Beshow95@gmail.com>
2025-02-11 18:56:52 +0300

    Other types

---
+++ data_types.kt
@@ -0,0 +1,10 @@
+fun main(){
+    var degree: Double = 36.5
+    var isItRaining: Boolean = false
+    var Character: Char = 'A'
+
+    println("The temperature is $degree degrees.\n" +
+            "Is it raining? $isItRaining\n" +
+            "The first letter of my name is $Character.")
+
+}
