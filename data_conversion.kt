093cc18d359c0c818c7a053923091dff48718fa6
Motlaq-Alnassafi <alnassafimotlaq@gmail.com>
2025-02-11 17:59:49 +0300

    Added data type conversion exercise

---
+++ data_conversion.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var numString = "100"
+    var numInt = numString.toInt()
+    var numInt2 = 50
+    var numString2 = numInt2.toString()
+
+    println("numInt is: $numInt")
+    println("numString2 is: $numString2")
+}
