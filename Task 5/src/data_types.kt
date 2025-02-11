0c594241ab549e461a698817edd7acc9664b5864
M1-49-mariam <mariam.almutairi19@gmail.com>
2025-02-11 19:25:58 +0300

    Finish tasks

---
+++ Task 5/src/data_types.kt
@@ -0,0 +1,6 @@
+fun main() {
+    var temperature = 36.5
+    var isRaining = false
+    var firstLetter = 'A'
+    println("The temperature is $temperature degrees. \n Is it raining? $isRaining \n The first letter of my name is $firstLetter")
+}
