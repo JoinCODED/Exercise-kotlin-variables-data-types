c926f70212e38a252cd12b882f248b5e7f5175a6
Ahmed Mandy <ahmedmandy@me.com>
2025-02-11 19:21:06 +0300

    Update data_types.kt
    Change boolean raining to isRaining

--- data_types.kt
+++ data_types.kt
@@ -1,6 +1,6 @@
 fun main() {
 	var temperature: Double = 36.5
-	var raining: Boolean = false
+	var isRaining: Boolean = false
 	var firstLetter: Char = 'A'
 	println("The temperature is $temperature degrees.\n Is it raining? $raining\n The first letter of my name is $firstLetter.")
 }
