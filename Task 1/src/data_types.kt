fa60d78ee512522db113502adc2258f69846e0bb
Yousef <yayalkandari@gmail.com>
2025-02-11 18:25:26 +0300

    removed comments

--- Task 1/src/data_types.kt
+++ Task 1/src/data_types.kt
@@ -1,17 +1,3 @@
-//1. Declare and initialize variables of the following types:
-//- `Double`: Store a floating-point number.
-//- `Boolean`: Store `true` or `false`.
-//- `Char`: Store a single letter.
-//2. Print each variable with a message describing its value.
-//
-//**Expected Output Example:**
-//
-//```kotlin
-//The temperature is 36.5 degrees.
-//Is it raining? false
-//The first letter of my name is A.
-//
-//```
 
 fun main() {
     var temperature: Double = 36.5;
--- Task 1/src/type_inference.kt
+++ Task 1/src/type_inference.kt
@@ -1,19 +1,5 @@
 import kotlin.reflect.typeOf
 
-// **Task:**
-
-// 1. Declare a variable `city` without specifying the type, and assign it `"New York"`.
-// 2. Declare a variable `temperature` without specifying the type, and assign it `22.5`.
-// 3. Print both variables and their inferred types using `.javaClass.kotlin`.
-
-// **Expected Output Example:**
-
-// ```kotlin
-// The type of city is class kotlin.String
-// The type of temperature is class kotlin.Double
-
-// ```
-
 fun main() {
 
     var city: String = "New York";
