4f919e7b9f9e0a9467f02c2efc940d5e605ad7a6
Mohamed Madooh <mmadooh14@gmail.com>
2025-02-11 19:21:50 +0300

    Done Task 5

---
+++ data_types.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var temperature = 36.5
+    var bool = false
+    var initial = 'M'
+
+    println("The temperature is $temperature degrees.")
+
+    println("Is it raining? $bool" )
+
+    println("The first letter of my name is  $initial" )
+}
