d15ef93b730dc6cc149313ae3287cf8ac3efc3c9
spdys <noorii549@yahoo.com>
2025-02-11 18:41:30 +0300

    Added Kotlin data types exercise

---
+++ data_types.kt
@@ -0,0 +1,8 @@
+fun main() {
+    var temperature:Double  = 36.5
+    var raining:Boolean  = false
+    var firstletter:Char = 'N'
+    println("The temperature is $temperature degrees.")
+    println("Is it raining? $raining")
+    println("The first letter of my name is $firstletter")
+}
