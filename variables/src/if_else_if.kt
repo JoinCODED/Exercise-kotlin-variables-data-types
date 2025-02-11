4bc135447fda0e039dceed8ec0b2e8b659ab3288
miskaldukhi <miskaldukhi@gmail.com>
2025-02-12 02:49:47 +0300

    if else part3

---
+++ variables/src/if_else_if.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var num: Int = 4
+    if (num > 0) {
+        println("The number is positive")
+    } else if (num < 0) {
+        println("The number is negative")
+    } else {
+        println("The number is zero")
+    }
+}
