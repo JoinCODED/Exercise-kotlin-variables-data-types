4aaf2ec42050025ed8762d38bc3de36dd57e235d
mohammedsheshtar <112146380+mohammedsheshtar@users.noreply.github.com>
2025-02-11 19:17:26 +0300

    main

---
+++ tasks/src/data_types.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var temperature: Double = 36.5;
+    var rainFall: Boolean = false;
+    var firstLetterName = 'A';
+
+    println("The temperature is $temperature degrees.");
+    println("Is it raining? $rainFall");
+    println("The first letter of my name is $firstLetterName");
+}
