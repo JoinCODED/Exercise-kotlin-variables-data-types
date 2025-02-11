f73c0bcd5235a060fa5984f920de2b4a1a27924b
alrweashd <trkyabler@gmail.com>
2025-02-11 18:53:06 +0300

    Last task

---
+++ data_types.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+
+    var temperature: Double = 36.5
+    var isRaining: Boolean = false;
+    var letter:Char = 'T';
+
+    // .javaClass.kotlin
+
+/*    The temperature is 36.5 degrees.
+    Is it raining? false
+    The first letter of my name is A.*/
+
+    println("The temperature is ${temperature} degrees." );
+    println("Is it raining? ${isRaining}");
+    println("The first letter of my name is ${letter}");
+
+
+}
