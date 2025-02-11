0df0d5e6c5690a1028f7357431deb106aa98c54c
miskaldukhi <miskaldukhi@gmail.com>
2025-02-11 20:33:28 +0300

    Part 5 Done

---
+++ variables/src/data_conversion.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var numString:String = "100"
+    val numInt:Int = numString.toInt()
+    var numInt2:Int = 50
+    val numString2:String = numInt2.toString()
+
+    println("numInt  is: $numInt")
+    println("numString2 is: $numString2")
+
+
+}
