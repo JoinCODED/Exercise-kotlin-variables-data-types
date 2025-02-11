252fc88feaa46790767a557a2ec8833725631a5b
alrweashd <trkyabler@gmail.com>
2025-02-11 18:33:49 +0300

    third task

---
+++ type_inference.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+
+    var city = "New York";
+    var temperature = 22.5;
+    val greeting:String = "Hello, Kotlin!"
+    //greeting="ljsfil"
+    // .javaClass.kotlin
+
+    println("The type of city is ${city.javaClass.kotlin}.");
+
+
+}
