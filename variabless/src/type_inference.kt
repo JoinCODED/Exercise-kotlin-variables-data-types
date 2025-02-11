a903e5d49f6a9c7843bdd2d8ade896262e16d75a
Dalal Bakhsh <DalalBakhsh98@gmail.com>
2025-02-11 19:07:00 +0300

    [200~Added type inference exercise~

---
+++ variabless/src/type_inference.kt
@@ -0,0 +1,8 @@
+fun main(){
+    var city:String = "New York"
+    var temperatue = 22.5
+
+    println("The type of city is  ${city.javaClass.kotlin} ")
+    println("The type of temperature is ${temperatue.javaClass.kotlin} ")
+
+}
