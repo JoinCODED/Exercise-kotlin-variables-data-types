9676129935162b6545da9966aec88d84225fcb91
Mohamed Madooh <mmadooh14@gmail.com>
2025-02-11 19:14:27 +0300

    Done Task 4

---
+++ data_conversion.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var numString = "100"
+    var numInt : Int  = numString.toInt()
+    var numInt2 = 50
+    var numString2 : String  = numInt2.toString()
+
+    println(numInt )
+    println(numString2 )
+
+}
