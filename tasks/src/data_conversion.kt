f77d7d22f4c6c877be359ee9f997f2202e55f44d
Yousef Alothman <yousefoalothman@gmail.com>
2025-02-11 18:58:02 +0300

    Added data type conversion exercise

---
+++ tasks/src/data_conversion.kt
@@ -0,0 +1,24 @@
+/*
+Declare a String variable numString and assign it "100".
+Convert numString to an Int and store it in numInt.
+Declare an Int variable numInt2 and assign it 50.
+Convert numInt2 to a String and store it in numString2.
+Print both converted values.
+ */
+
+fun main(){
+
+        val numString: String = "100"
+        val numInt: Int = numString.toInt()
+
+        val numInt2: Int = 50
+        val numString2: String = numInt2.toString()
+
+        println("Converted numString to Int: $numInt")
+        println("Converted numInt2 to String: $numString2")
+
+
+
+
+
+}
