6e843f5f2d4a37d5fbe5cc3b86193f3f69b48685
Njood <nejood03@gmail.com>
2025-02-12 08:27:25 +0300

    Implemented program to find the largest of three numbers

---
+++ Variable/src/largest_number.kt
@@ -0,0 +1,14 @@
+fun main () {
+    val a = 3
+    val b = 9
+    val c = 2
+
+    if (a >= b && a >= c) {
+            println("The largest number is $a")
+        } else if (b >= a && b >= c) {
+            println("The largest number is $b")
+        } else {
+            println("The largest number is $c")
+        }
+    }
+
