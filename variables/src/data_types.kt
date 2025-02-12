0c7dc47cacaefffe7859fa9f214d42efeb41a4ee
A <alzemamia@gmail.com>
2025-02-12 07:44:16 +0300

    part5 done

---
+++ variables/src/data_types.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    var temperature: Double = 36.5
+
+    var isRaining: Boolean = false
+
+    var firstLetter: Char = 'A'
+
+    println("The temperature is $temperature degrees.")
+    println("Is it raining? $isRaining")
+    println("The first letter of my name is $firstLetter.")
+
+}
