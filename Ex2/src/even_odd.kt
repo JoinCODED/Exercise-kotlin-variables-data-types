a1882e33e2625f03356d5c2f30dc2b9defe945b2
amalal22 <amalalzabi2@gmail.com>
2025-02-12 17:10:02 +0300

    Ex3

---
+++ Ex2/src/even_odd.kt
@@ -0,0 +1,12 @@
+fun main()
+{
+    var num = 5
+    if (num %2 == 0)
+    {
+        println("Number is even")
+
+    }
+    else {
+        println("Number is odd")
+    }
+}
---
+++ Ex3/src/greetTraveler.kt
@@ -0,0 +1,24 @@
+fun main ()
+{
+    println( greetTraveler())
+    println(findOasis(10.0,20.0))
+}
+
+
+fun greetTraveler():String {
+    return("Welcome to the Desert, Traveler!")
+}
+
+fun findOasis(x:Double , y:Double):String
+{
+    return("Oasis found at coordinates ($x, $y)")
+}
+
+fun currentTemperature(morningTemperature: Int , afternoonTemperature: Int): String
+{
+
+    var v: Double
+
+    v = (morningTemperature.toDouble()+afternoonTemperature.toDouble())/2
+    return("Average temp is $v")
+}
