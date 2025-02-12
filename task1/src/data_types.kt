fa15a3a78a08d657c9bbf586c39597d223aab7e7
Sarah <sarahalenezi96@outlook.com>
2025-02-12 12:02:33 +0300

    Added Kotlin data types exercise

---
+++ task1/src/data_types.kt
@@ -0,0 +1,6 @@
+fun main(){
+    var temp = 37.4
+    var rain = true
+    var name = 'S'
+    println ("The temperature is $temp degrees\n Is it raining? $rain \n The first letter of my name is $name ")
+}
